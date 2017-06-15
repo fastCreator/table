24337121399f17874f9d1269fbd5bc43d9200f56
张果 <237625092@qq.com>
2017-06-15 18:14:43 +0800

    .

--- src/components/tableHeader.vue
+++ src/components/tableHeader.vue
@@ -2,7 +2,7 @@
    <div class="table-header">
           <table border="1">
             <tr v-for="(item ,dep) in header">
-              <th v-for="i in item" :colspan=" 1" :rowspan=" 1">{{i.label}}</th>
+              <th v-for="i in item" :colspan="i.width" :rowspan="header.length-dep-Object.keys(i.deep).length">{{i.label}}</th>
             </tr>
           </table>
    </div>
@@ -18,9 +18,8 @@ export default {
   props:['data'],
   computed:{
     header(){
-      console.log(getHeader({child:this.data}));
-      return [];
-       //return getHeader({child:this.data});
+      console.log(getHeader({child:this.data,deep:{}}));
+      return getHeader({child:this.data,deep:{},width:0});
     }
   },
   methods:{
@@ -31,16 +30,33 @@ export default {
 function getHeader(data,arr=[],i=0){
     console.log(data);
     if(!arr[i]){arr[i]=[];}
-
     data.child.forEach(function(item){
         arr[i].push(item);
         item.parent= data;
-        if(!item.deep){item.deep=[];};
+        item.deep={};
+        item.width =0;
+        addPDep(item,i);
         if(item.child){
           getHeader(item,arr,++i);
+        }else{
+          addPWidth(item);
         }
     });
     return arr;
+
+    function addPDep(item,i){
+        if(item.parent){
+          item.parent.deep[i]=true;
+          addPDep(item.parent,i)
+        }
+    }
+
+    function addPWidth(item){
+        if(item.parent){
+          item.parent.width++;
+          addPWidth(item.parent);
+        }
+    }
 }
 
 
